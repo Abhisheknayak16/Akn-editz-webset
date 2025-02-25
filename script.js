4aa9ba6c2f433fca64cb66b47facaae37ae57c9a
Abhisheknayak16 <anayak8543@gmail.com>
2025-02-25 15:49:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+const themeToggle = document.getElementById("theme-toggle");
+   const body = document.body;
+
+   // Check localStorage for theme preference
+   if (localStorage.getItem("theme") === "light") {
+     body.classList.add("light-mode");
+     themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+   }
+
+   // Toggle Theme
+   themeToggle.addEventListener("click", () => {
+     body.classList.toggle("light-mode");
+     if (body.classList.contains("light-mode")) {
+       localStorage.setItem("theme", "light");
+       themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+     } else {
+       localStorage.setItem("theme", "dark");
+       themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+     }
+   });
